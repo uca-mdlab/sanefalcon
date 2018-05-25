83f45b11fc2d92d900e513bc49cb2cafd82180a0
marcomilanesio <milanesio.marco@gmail.com>
2018-05-25 16:40:49 +0200

    create last available directory under train root dir in alphabetic order

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+import string
+
+import random
+
+letters = list(string.ascii_lowercase)
+parser = argparse.ArgumentParser(description='Prepare the environment and start the plugin',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
+parser.add_argument('traindir', type=str, default="/tmp", help='path of the train subtree')
+
+args = parser.parse_args()
+
+bamdir = args.bamdir
+traindir = args.traindir
+
+files_to_link = []
+for root, subdir, files in os.walk(bamdir):
+    for f in files:
+        files_to_link.append(os.path.join(root, f))
+
+print(files_to_link)
+
+try:
+    lst = sorted(os.listdir(traindir))[-1]
+except IndexError:
+    lst = None
+
+if not lst:
+    os.mkdir(os.path.join(traindir, 'a'))
+else:
+    os.mkdir(os.path.join(traindir, letters[letters.index(lst) + 1 % len(letters)]))
