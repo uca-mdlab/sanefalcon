d51a5db938a260d455bfb01e8dab8b6d3cdd6ea5
marcomilanesio <milanesio.marco@gmail.com>
2018-06-01 11:21:33 +0200

    comment for test

--- merge.sh
+++ merge.sh
@@ -5,7 +5,7 @@ ISQSUB=`command -v qsub`
 
 
 if [[ $ISQSUB -eq 0 ]]; then
-    for i in $(seq 1 22)
+    for i in $(seq 1 3)  # for testing locally only (change to 1 22)
     do
 	    echo Working on Chr: $i
         sort -n -m $INDIR/*/*.$i.start.fwd $INDIR/*/*.$i.start.rev > $INDIR/merge.$i
