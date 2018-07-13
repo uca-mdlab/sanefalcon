c4f44138b9d3c3f81759aa4e2df0d282bc85f52f
marcomilanesio <milanesio.marco@gmail.com>
2018-07-13 16:44:56 +0200

    add filter to get useful bam

--- prepare_folders.py
+++ prepare_folders.py
@@ -1,30 +1,68 @@
 import os
 import argparse
 import string
+import re
+
+import time
+import datetime
 
 import random
 
 letters = list(string.ascii_lowercase)
-parser = argparse.ArgumentParser(description='Prepare the environment and start the plugin',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
-parser.add_argument('traindir', type=str, default="/tmp", help='path of the train subtree')
+# parser = argparse.ArgumentParser(description='Prepare the environment and start the plugin',
+#                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#
+# parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
+# parser.add_argument('traindir', type=str, default="/tmp", help='path of the train subtree')
+#
+# args = parser.parse_args()
+# bamdir = args.bamdir
+# traindir = args.traindir
+bamdir = "/home/marco/temp/sanefalcon_bam"
+traindir = "/home/marco/temp/train"
 
-args = parser.parse_args()
 
-bamdir = args.bamdir
-traindir = args.traindir
+def list_files_to_use(bamdir):
+    """
 
-files_to_link = []
-for root, subdir, files in os.walk(bamdir):
-    for f in files:
-        files_to_link.append(os.path.join(root, f))
+    :param bamdir: the root of all manips
+    :return: list of bam files to use for training
+    :return: list of manip names
+    """
+    first_date = datetime.date(2017, 1, 13)
+    timestamp = time.mktime(first_date.timetuple())
 
+    exclude = set([d for d in os.listdir(bamdir) if re.search('_tn_', d) or not re.search('Auto_user_', d) or
+                   not re.search('DPNI', d) or os.path.getmtime(os.path.join(bamdir, d)) <= timestamp])
+
+    files_to_link = []
+    for root, subdir, files in os.walk(bamdir):
+        subdir[:] = set(subdir) - exclude
+        for f in files:
+            if f.endswith(".bam"):
+                files_to_link.append(os.path.join(root, f))
+            if root.count(os.sep) >= 1:  # stop at first level
+                del subdir[:]
+
+    # print(len(files_to_link))
+    for i in files_to_link:
+        print(i)
+
+    manip_list = list(set([f.split('/')[-2] for f in files_to_link]))  # list of manip with .bam files
+    # print(manip_list)
+    return files_to_link, manip_list
+
+
+files_to_link, manip_list = list_files_to_use(bamdir)
+
+print("{} bam files in {} manips".format(len(files_to_link), len(manip_list)))
 try:
     lst = sorted(os.listdir(traindir))[-1]
 except IndexError:
     lst = None
+
+exit(0)
 
 if not lst:
     workingdir = os.path.join(traindir, 'a')
