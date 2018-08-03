50e8f6044f580bc8160cc0c03967f6be00bfdf9c
marcomilanesio <milanesio.marco@gmail.com>
2018-08-03 15:15:16 +0200

    added test for "position file found" for incremental preparation

--- prepSamples.sh
+++ prepSamples.sh
@@ -22,20 +22,22 @@ do
 	
 	for ARG_TASKID in `seq 1 2` # or "X"
 		do
-			$SCRIPT_SAMTOOLS view \
-			$SAMPLE \
-			chr$ARG_TASKID \
-			-F 20 -q 1 | \
-			$SCRIPT_PYTHON $SCRIPT_RETRO | \
-				awk '{print $4}' \
-					    > $OUTFILE.$ARG_TASKID.start.fwd &
+		    if [ ! -f $OUTFILE.$ARG_TASKID.start.fwd ]; then
+			    $SCRIPT_SAMTOOLS view \
+			    $SAMPLE \
+			    chr$ARG_TASKID \
+			    -F 20 -q 1 | \
+			    $SCRIPT_PYTHON $SCRIPT_RETRO | \
+				    awk '{print $4}' \
+					        > $OUTFILE.$ARG_TASKID.start.fwd &
 
-			$SCRIPT_SAMTOOLS view \
-			$SAMPLE \
-			chr$ARG_TASKID \
-			-f 16 -F 4 -q 1 | \
-			$SCRIPT_PYTHON $SCRIPT_RETRO | \
-					awk '{print ($4 + length($10) - 1)}' \
-					    > $OUTFILE.$ARG_TASKID.start.rev
+                $SCRIPT_SAMTOOLS view \
+                $SAMPLE \
+                chr$ARG_TASKID \
+                -f 16 -F 4 -q 1 | \
+                $SCRIPT_PYTHON $SCRIPT_RETRO | \
+                        awk '{print ($4 + length($10) - 1)}' \
+                            > $OUTFILE.$ARG_TASKID.start.rev
+            fi
 		done
 done
