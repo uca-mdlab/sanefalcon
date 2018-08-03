e945b24452f26a2c4bfa5933f95bec0ab50fd865
marcomilanesio <milanesio.marco@gmail.com>
2018-08-03 13:39:48 +0200

    logging + main function

--- prepare_folders.py
+++ prepare_folders.py
@@ -7,19 +7,13 @@ import time
 import datetime
 
 import random
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
+                    filename='sanefalcon.log', filemode='w', level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
 
 letters = list(string.ascii_lowercase)
-
-parser = argparse.ArgumentParser(description='Prepare the environment and start the plugin',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
-parser.add_argument('traindir', type=str, default="/tmp/sanefalcontrain", help='path of the train subtree')
-
-args = parser.parse_args()
-bamdir = args.bamdir
-traindir = args.traindir
-
 
 
 def list_files_to_use(bamdir):
@@ -44,11 +38,8 @@ def list_files_to_use(bamdir):
             if root.count(os.sep) >= 1:  # stop at first level
                 del subdir[:]
 
-    # print(len(files_to_link))
-    # for i in files_to_link:
-    #     print(i)
-
     manip_list = list(set([f.split('/')[-2] for f in files_to_link]))  # list of manip with .bam files
+    logger.info("Found {} manip with {} bam files".format(len(manip_list), len(files_to_link)))
     return files_to_link, manip_list
 
 
@@ -58,35 +49,47 @@ def prepare_batch_folders(manip_list):
         yield manip_list[i:i + 5]
 
 
-files_to_link, manip_list = list_files_to_use(bamdir)
+def main(bamdir, traindir):
+    files_to_link, manip_list = list_files_to_use(bamdir)
 
-print("{} bam files in {} manips".format(len(files_to_link), len(manip_list)))
+    batches = {}
+    for num_batch, batch in enumerate(prepare_batch_folders(manip_list)):
+        batches[letters[num_batch]] = batch
+        logger.debug("Batch {}: {}".format(letters[num_batch], batch))
 
-for batch in prepare_batch_folders(manip_list):
-    print('batch')
-    print(batch)
+    for batch_name, batch_list in batches.items():
+        workingdir = os.path.join(traindir, batch_name)
+        try:
+            os.mkdir(workingdir)
+            logger.debug("Created folder: {}".format(workingdir))
+        except FileExistsError:
+            logger.warning("Folder {} exists, skipping...".format(workingdir))
 
-try:
-    lst = sorted(os.listdir(traindir))[-1]
-except IndexError:
-    lst = None
+        for manip in batch_list:
+            manip_regex = re.compile(manip)
+            files = [f for f in files_to_link if re.search(manip_regex, f)]
+            logger.debug("Batch {}: {}".format(batch_name, files))
+            runpath = ''
+            for fname in files:
+                run = fname.split(bamdir)[1].split('/')[1]
+                runpath = os.path.join(workingdir, run)
+                if not os.path.isdir(runpath):
+                    os.mkdir(runpath)
+                os.symlink(fname, os.path.join(runpath, fname.split('/')[-1]))
+        logger.info("Batches created with symlinks")
 
-exit(0)
 
-if not lst:
-    workingdir = os.path.join(traindir, 'a')
-else:
-    workingdir = os.path.join(traindir, letters[letters.index(lst) + 1 % len(letters)])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Prepare the environment and start the plugin',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-os.mkdir(workingdir)
-# print('Training subfolder created')
+    parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
+    parser.add_argument('traindir', type=str, default="/tmp/sanefalcontrain", help='path of the train subtree')
 
-runpath = ''
-for fname in files_to_link:
-    run = fname.split(bamdir)[1].split('/')[1]
-    runpath = os.path.join(workingdir, run)
-    if not os.path.isdir(runpath):
-        os.mkdir(runpath)
-    os.symlink(fname, os.path.join(runpath, fname.split('/')[-1]))
+    args = parser.parse_args()
+    bamdir = args.bamdir
+    traindir = args.traindir
 
-print(runpath)
+    main(bamdir, traindir)
+    print(traindir)
+
