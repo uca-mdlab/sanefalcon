a807fb7094d27046e6f43ef53a985d8bf6ea49e8
marcomilanesio <milanesio.marco@gmail.com>
2018-08-03 15:10:22 +0200

    - basename - seq 1-2 for testing

--- prepSamples.sh
+++ prepSamples.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 INDIR=$1
 OUTDIR=$2
-# mkdir $OUTDIR
 
-DIR_FETALFRAC=/home/davidpratella/fetalfrac
+
+#DIR_FETALFRAC=/home/davidpratella/fetalfrac
 SCRIPT_RETRO=./retro.py
 
-DIR_SCRIPTS=$DIR_FETALFRAC
+#DIR_SCRIPTS=$DIR_FETALFRAC
 #export DIR_SCRIPTS=/illumina/diagnostics/bin
 #export SCRIPT_PYTHON=/illumina/diagnostics/bin/Python-2.7.3/bin/python
-SCRIPT_PYTHON=/usr/local/bin/python3
+SCRIPT_PYTHON=/usr/bin/python3
 
 SCRIPT_SAMTOOLS=samtools
 
 for SAMPLE in `find $INDIR -name "*.bam"`
 do
-	SHORT=`echo $SAMPLE | rev | cut -d"/" -f1 | rev`
+#	SHORT=`echo $SAMPLE | rev | cut -d"/" -f1 | rev`
+	SHORT=`echo $(basename $SAMPLE)`
 	OUTFILE="$OUTDIR/${SHORT//".sort.bam"/}"
 	echo "IN:$SAMPLE, OUT:$OUTFILE"
 	
-	for ARG_TASKID in `seq 1 5` # or "X"
+	for ARG_TASKID in `seq 1 2` # or "X"
 		do
 			$SCRIPT_SAMTOOLS view \
 			$SAMPLE \
