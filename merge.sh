2a46db2fc36cc1df9b73d4b1ced44eb943d04655
ionadmin <ionadmin@3Q8QQV1>
2018-08-22 16:15:08 +0200

    modification par david

--- merge.sh
+++ merge.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-INDIR=`greadlink -f $1`
+INDIR=`readlink -f $1`
 
 for i in $(seq 1 22)
 do
 	echo Working on Chr: $i
-	#echo "sort -n -m $INDIR/*/*.$i.start.fwd $INDIR/*/*.$i.start.rev > $INDIR/merge.$i" | qsub
-	sort -n -m $INDIR/*/*.$i.start.fwd $INDIR/*/*.$i.start.rev > $INDIR/merge.$i
+	echo "sort -n -m $INDIR/*/*.$i.start.fwd $INDIR/*/*.$i.start.rev > $INDIR/merge.$i" | qsub
+	#sort -n -m $INDIR/*/*.$i.start.fwd $INDIR/*/*.$i.start.rev > $INDIR/merge.$i
 done
--- mergeAntiSub.sh
+++ mergeAntiSub.sh
@@ -3,7 +3,7 @@
 INDIR=`readlink -f $1`
 SUB=$2
 
-for CHROM in $(seq 3 -1 1)  # local testing replace 3->22
+for CHROM in $(seq 22 -1 1)  # local testing replace 3->22
 do
 	COLLECT=`ls $INDIR/*/merge.$CHROM | grep -v "$SUB/merge"`
 	sort -n -m $COLLECT > $INDIR/$SUB/anti.$CHROM #  #" | qsub
