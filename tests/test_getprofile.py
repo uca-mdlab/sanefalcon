fca2b7e4e8a5132f14a310b6e6dc6f11eca4b2e3
marcomilanesio <milanesio.marco@gmail.com>
2018-10-25 13:41:46 +0200

    added first test for getProfile original script

---
+++ tests/test_getprofile.py
@@ -0,0 +1,37 @@
+import unittest
+import getProfileParallel
+import os
+import sys
+
+
+class TestGetProfile(unittest.TestCase):
+    def setUp(self):
+        self.scriptname = 'getProfile.py'
+        self.chromfilefwd = './tests/chromfile.fwd'
+        self.chromfilerev = './tests/chromfile.rev'
+        self.nuclfile = './tests/nuclfile'
+        self.configurations = {
+            'fwd0': {'outfile': '{}.fwd'.format(self.chromfilefwd), 'rev': 0},
+            'fwd1': {'outfile': '{}.ifwd'.format(self.chromfilefwd), 'rev': 1},
+            'rev0': {'outfile': '{}.irev'.format(self.chromfilerev), 'rev': 0},
+            'rev1': {'outfile': '{}.rev'.format(self.chromfilerev), 'rev': 1},
+        }
+
+    def test_fwd0(self):
+        outfile = self.configurations['fwd0']['outfile']
+        rev = self.configurations['fwd0']['rev']
+        result = os.system("python3 {} {} {} {} {}".format(self.scriptname, self.nuclfile, self.chromfilefwd, rev, outfile))
+        self.assertEqual(result, 0)
+        with open(outfile, 'r') as resfile:
+            arr = resfile.readlines()[0].split(',')
+
+        self.assertEqual(arr[0].strip(), '270.0')
+        self.assertEqual(arr[-1].strip(), '297.0')
+        # now check that the script's output is what you expect
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
