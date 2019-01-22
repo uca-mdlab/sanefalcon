be25e6d85525bbf8979a4f43aec66be6f534c9d8
davidpratella <david.pratella@unice.fr>
2019-01-22 11:54:37 +0100

    modifier getProfileParallel.py l.268

--- nuclDetector.sh
+++ nuclDetector.sh
@@ -1,11 +1,12 @@
-export SCRIPT_NUCDEC=~/sanefalcon/nuclDetector.py
+export SCRIPT_NUCDEC=nuclDetector.py
 
-export SCRIPT_PYTHON=/illumina/diagnostics/bin/Python-2.7.3/bin/python
+export SCRIPT_PYTHON=python
 
 export INDIR=`readlink -f $1`
 
 for CHROM in $(seq 22 -1 1)
 do
-	echo "$SCRIPT_PYTHON $SCRIPT_NUCDEC $INDIR/merge.$CHROM $INDIR/nucl_ex4.$CHROM" | qsub
+#	echo "$SCRIPT_PYTHON $SCRIPT_NUCDEC $INDIR/merge.$CHROM $INDIR/nucl_ex4.$CHROM" | qsub
+	$SCRIPT_PYTHON $SCRIPT_NUCDEC $INDIR/merge.$CHROM $INDIR/nucl_ex4.$CHROM
 done
 
