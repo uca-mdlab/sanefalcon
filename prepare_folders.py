7083b8eeecf81ad602eeddd84d67859280f6a59a
davidpratella <david.pratella@unice.fr>
2019-02-20 15:59:52 +0100

    add argparse, new option : -n for nucltrack selection

--- prepare_folders.py
+++ prepare_folders.py
@@ -13,6 +13,36 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
 
 logger = logging.getLogger(__name__)
 
+def readfile(bamlist):
+    with open (bamlist) as f:
+        myfile=[x.strip() for x in f.readlines()]
+    f.close()
+
+    return myfile
+
+def list_files_to_use2(bamlist):
+    dict_run={}
+    for x in bamlist:
+        run,file=x.split(';')
+        print(file,"file")
+        print(run,'run')
+        # run=x.split(';')[1]
+        if run not in dict_run:
+            dict_run[run]=[]
+            dict_run[run].append(file)
+
+        else:
+            dict_run[run].append(file)
+
+    run,file=[x for k,v in dict_run.items()]
+
+    for k,v in dict_run.items():
+         print(k,v)
+
+    return run,file
+
+
+
 
 def list_files_to_use(bamdir):
     """
@@ -21,15 +51,17 @@ def list_files_to_use(bamdir):
     :return: list of bam files to use for training
     :return: list of manip names
     """
+
     first_date = datetime.date(2017, 1, 13)
     timestamp = time.mktime(first_date.timetuple())
 
-    exclude = set([d for d in os.listdir(bamdir) if re.search('_tn_', d) or not re.search('Auto_user_', d) or
-                   not re.search('DPNI', d) or os.path.getmtime(os.path.join(bamdir, d)) <= timestamp])
+    # exclude = set([d for d in os.listdir(bamdir) if re.search('_tn_', d) or not re.search('Auto_user_', d) or
+    #                not re.search('DPNI', d) or os.path.getmtime(os.path.join(bamdir, d)) <= timestamp])
 
     files_to_link = []
     for root, subdir, files in os.walk(bamdir):
-        subdir[:] = set(subdir) - exclude
+        # subdir[:] = set(subdir) - exclude
+        subdir[:] = set(subdir)
         for f in files:
             if f.endswith(".bam") or f.endswith(".bai"):
                 files_to_link.append(os.path.join(root, f))
@@ -42,13 +74,15 @@ def list_files_to_use(bamdir):
 
 
 def prepare_batches(manip_list):
-    """Yield successive 5-sized chunks from manip_list."""
+    # Yield successive 5-sized chunks from manip_list.
     for i in range(0, len(manip_list), 5):
         yield manip_list[i:i + 5]
 
 
 def prepare_train_folder(bamdir, traindir):
+# def prepare_train_folder(bamlist, traindir):
     letters = list(string.ascii_lowercase)
+    # files_to_link,manip_list=list_files_to_use2(bamlist)
     files_to_link, manip_list = list_files_to_use(bamdir)
 
     batches = {}
@@ -83,12 +117,17 @@ if __name__ == '__main__':
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('bamdir', type=str, default="/results/analysis/output/Home", help='path of the manipulation')
+    # parser.add_argument('bamlist', type=str, help='list of bamfiles')
     parser.add_argument('traindir', type=str, default="/tmp/sanefalcontrain", help='path of the train subtree')
 
     args = parser.parse_args()
     bamdir = args.bamdir
+    # bamlist = args.bamlist
     traindir = args.traindir
-
+    #
     prepare_train_folder(bamdir, traindir)
-    print(traindir)
+    # print(traindir)
+
+
+    # print(list_files_to_use2(readfile(bamlist)))
 
