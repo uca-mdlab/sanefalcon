e665a4c03be5d6cce2645316f4b4c9e17920fef7
davidpratella <david.pratella@unice.fr>
2019-02-21 09:49:02 +0100

    add argparse, new option : -n for nucltrack selection

--- prepare_folders_david.py
+++ prepare_folders_david.py
@@ -47,7 +47,7 @@ def list_files_to_use(bamdir):
 
     manip_list = list(set([f.split('/')[-2] for f in bamdir]))  # list of manip with .bam files
     logger.info("Found {} manip with {} bam files".format(len(manip_list), len(bamdir)))
-    return files_to_link, manip_list
+    return bamdir, manip_list
 
 
 
