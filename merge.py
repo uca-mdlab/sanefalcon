eae4598876ef764ed64e1a6bbdaeb9b3de6bb310
marcomilanesio <milanesio.marco@gmail.com>
2019-02-28 15:34:29 +0100

    testing merge files

--- merge.py
+++ merge.py
@@ -185,9 +185,9 @@ if __name__ == "__main__":
     dic = prepare_file_lists(trainfolder)
     print('manips')
     for k, v in manips.items():
-        print(k, v)
+        print(k, sorted(v))
     # merge_all(trainfolder)
     print('dic')
     for k, v in dic.items():
-        print(k, v)
+        print(k, sorted(v))
     # merge(dic)
