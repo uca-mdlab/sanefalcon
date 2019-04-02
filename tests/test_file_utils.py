9d47f1df0fb86e8d82bfccf352b6ba45936b62a9
marcomilanesio <milanesio.marco@gmail.com>
2019-04-02 13:15:42 +0200

    test for batch sizes as a parameter
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -15,6 +15,22 @@ class TestFileUtils(unittest.TestCase):
         self.assertEqual(len(lines), 173875)
         self.assertEqual(type(lines[0]), str)
 
+    def test_preparebatches(self):
+        l = ['a', 'b', 'c', 'd', 'e', 'f']
+        for i in range(1, len(l)):
+            num_odd_chunks = 0
+            res = list(Utils.prepare_batches(l, i))
+            try:
+                # evenly sized chunks
+                self.assertTrue(all([len(x) == i for x in res]))
+            except:
+                # only 1 chunk has different length
+                num_odd_chunks += 1
+            if num_odd_chunks:
+                self.assertEqual(num_odd_chunks, 1)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
