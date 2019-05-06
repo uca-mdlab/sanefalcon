c7bf8ab1e9ff017456ff6731c88aea9f030ad1de
marcomilanesio <milanesio.marco@gmail.com>
2019-05-06 11:50:29 +0200

    skipping if nucleosome file is already there
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- nucleosome_detector.py
+++ nucleosome_detector.py
@@ -105,7 +105,11 @@ def assemble_runs(trainfolder, files, file_template, subdirs=None):
 
 
 def _create_nucleosome_file(folder, chrom, mergefile, fname):
-    logger.info("{} creating nucleosome file for {}".format(os.getpid(), mergefile))
+    outfile = os.path.join(folder, fname + ".{}".format(chrom))
+    if os.path.isfile(outfile):
+        logger.info("nucleosome file for {} = {} already there. Skipping..".format(mergefile, outfile))
+        return
+    logger.info("{} creating nucleosome file for {} = {}".format(os.getpid(), mergefile, outfile))
     curArea = [0]
     lastPos = 0
     maxDist = 190  # A little over our sliding window size
