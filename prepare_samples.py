b127cdec9c01f6a370735d5354ecdf374e81c950
marcomilanesio <milanesio.marco@gmail.com>
2019-05-06 10:45:38 +0200

    multithreaded
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- prepare_samples.py
+++ prepare_samples.py
@@ -5,7 +5,8 @@ import os
 import subprocess
 import sys
 import logging
-
+from threading import Thread
+import concurrent.futures
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
                     filename='sanefalcon.log', filemode='w', level=logging.DEBUG)
@@ -13,8 +14,29 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
 logger = logging.getLogger("prepare_samples")
 
 
+def prepare_fwd(samtools, bamfile, outfile, chrom):
+    one = [samtools, "view", bamfile, "chr{}".format(chrom), "-F", "20", "-q", "1"]
+    two = [sys.executable, "retro.py", "-fwd"]
+    p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=open(outfile, 'w'))
+    output = p2.communicate()[0]
+
+
+def prepare_rev(samtools, bamfile, revoutfile, chrom):
+    one = [samtools, "view", bamfile, "chr{}".format(chrom), "-f", "16", "-F", "4", "-q", "1"]
+    two = [sys.executable, "retro.py", "-rev"]
+    p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=open(revoutfile, 'w'))
+    output = p2.communicate()[0]
+
+
 def prepare_samples(datafolder, rspfolder, samtools):
+    if not os.path.isdir(rspfolder):
+        os.makedirs(rspfolder)
     chromosomes = range(1, 23)
+    fwd_jobs = []
+    rev_jobs = []
+    threads = []
 
     for root, subdir, files in os.walk(datafolder):
         for fname in files:
@@ -23,40 +45,43 @@ def prepare_samples(datafolder, rspfolder, samtools):
                 outfile_name = fname.split('.')[0] + '.sort.bam'
                 for chrom in chromosomes:
                     outfile = os.path.join(rspfolder, outfile_name + '.{}.start.fwd'.format(chrom))
+                    revoutfile = os.path.join(rspfolder, outfile_name + '.{}.start.rev'.format(chrom))
                     if not os.path.isfile(outfile):
-                        one = [samtools, "view", bamfile, "chr{}".format(chrom), "-F", "20", "-q" "1"]
-                        # two = [sys.executable, "retro.py"]
-                        two = [sys.executable, "retro.py", "-fwd"]
-                        # three = ["awk", "{print $4}"]
-                        p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
-                        # p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=subprocess.PIPE)
-                        p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=open(outfile, 'w'))
-                        # p3 = subprocess.Popen(three, stdin=p2.stdout, stdout=open(outfile, 'w'))
-                        # output = p3.communicate()[0]
-                        output = p2.communicate()[0]
+                        run = (samtools, bamfile, outfile, chrom)
+                        fwd_jobs.append(run)
 
-                        revoutfile = os.path.join(rspfolder, outfile_name + '.{}.start.rev'.format(chrom))
-                        one = [samtools, "view", bamfile, "chr{}".format(chrom), "-f", "16", "-F", "4", "-q" "1"]
-                        two = [sys.executable, "retro.py", "-rev"]
-                        # three = ["awk", "{print ($4 + length($10) - 1)}"]
-                        p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
-                        # p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=subprocess.PIPE)
-                        # p3 = subprocess.Popen(three, stdin=p2.stdout, stdout=open(revoutfile, 'w'))
-                        # output = p3.communicate()[0]
-                        p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=open(revoutfile, 'w'))
-                        output = p2.communicate()[0]
+                    if not os.path.isfile(revoutfile):
+                        run = (samtools, bamfile, revoutfile, chrom)
+                        rev_jobs.append(run)
 
                 logger.debug("prep_samples for {}".format(fname))
 
+    logger.info('Spawning {} threads'.format(len(fwd_jobs) + len(rev_jobs)))
+    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        jobs = {}
+        for job in fwd_jobs:
+            sub = executor.submit(prepare_fwd, *job)
+            jobs[sub] = job
+
+        for job in rev_jobs:
+            sub = executor.submit(prepare_rev, *job)
+            jobs[sub] = job
+
+        for job in concurrent.futures.as_completed(jobs):
+            _ = job.result()
+
+    logger.info('End multithreading. Samples prepared.')
+
 
 if __name__ == "__main__":
-    conf_file = 'sanefalcon.conf'
+    conf_file = 'tests/data/test.conf'
     config = configparser.ConfigParser()
     config.read(conf_file)
 
     samtools = config['default']['samtools']
     datafolder = config['default']['datafolder']
     trainfolder = config['default']['trainfolder']
-    nucleosomefolder = config['default']['nucleosomefolder']
+    rspfolder = config['default']['rspfolder']
+    # nucleosomefolder = config['default']['nucleosomefolder']
 
-    prepare_samples(datafolder, trainfolder, samtools)
+    prepare_samples(datafolder, rspfolder, samtools)
