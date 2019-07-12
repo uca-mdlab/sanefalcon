bd286839318c346dbdf53c6f30d696a3b0219fd6
marcomilanesio <milanesio.marco@gmail.com>
2019-07-12 15:03:56 +0200

    reverting upstream.

--- combine_profiles.py
+++ combine_profiles.py
@@ -13,22 +13,25 @@ def sum_profile_file(proffile):
     return s
 
 
-def get_profile_files_per_chrom(fm):
+def get_profile_files_per_chrom(fm, stream='up'):
     fnames = [os.path.join(fm.profilefolder, fname) for fname in os.listdir(fm.profilefolder)]
     for chrom in range(1, 23):
         pattern = re.compile(r"\.{}\.".format(chrom))
         chrom_files = list(filter(lambda x: re.search(pattern, x), fnames))
-        fwd = [f for f in chrom_files if f.endswith('.fwd')]
-        rev = [f for f in chrom_files if f.endswith('.rev')]
-        ifwd = [f for f in chrom_files if f.endswith('.ifwd')]
-        irev = [f for f in chrom_files if f.endswith('.irev')]
-        yield chrom, fwd, rev, ifwd, irev
+        if stream == 'up':
+            fwd = [f for f in chrom_files if f.endswith('.fwd')]
+            rev = [f for f in chrom_files if f.endswith('.rev')]
+            yield chrom, fwd, rev
+        else:
+            ifwd = [f for f in chrom_files if f.endswith('.ifwd')]
+            irev = [f for f in chrom_files if f.endswith('.irev')]
+            yield chrom, ifwd, irev
 
 
-def do_it(fwd):
+def combine_profile_files(arr):
     d = {}
     pattern = re.compile(r"\w*\.bam")
-    for fname in fwd:
+    for fname in arr:
         name = re.match(pattern, os.path.basename(fname)).group()
         d[name] = sum_profile_file(fname)
     return d
@@ -36,14 +39,40 @@ def do_it(fwd):
 
 def create_upstream(fm):
     res = defaultdict(dict)
-    for chrom, fwd, rev, _, _ in get_profile_files_per_chrom(fm):
-        d1 = do_it(fwd)
-        d2 = do_it(rev)
+    for chrom, fwd, rev in get_profile_files_per_chrom(fm, stream='up'):
+        d1 = combine_profile_files(fwd)
+        d2 = combine_profile_files(rev)
         c = Counter(d1)
         c.update(Counter(d2))
         for k, v in dict(c).items():
             res[k].update({chrom: v})
     return res
+
+
+def create_downstream(fm):
+    res = defaultdict(dict)
+    for chrom, ifwd, irev in get_profile_files_per_chrom(fm, stream='down'):
+        d1 = combine_profile_files(ifwd)
+        d2 = combine_profile_files(irev)
+        c = Counter(d1)
+        c.update(Counter(d2))
+        for k, v in dict(c).items():
+            res[k].update({chrom: v})
+    return res
+
+
+def create_streams(fm):
+    streams = {}
+    upstream = create_upstream(fm)
+    downstream = create_downstream(fm)
+    for k, v in upstream.items():
+        nums = [x for _, x in v.items()]
+        nums.reverse()
+        streams[k] = nums[:-1]
+    for k, v in downstream.items():
+        nums = [x for _, x in v.items()]
+        streams[k].extend(nums)
+    return streams
 
 
 if __name__ == '__main__':
@@ -57,6 +86,7 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('tests/data/test.conf')
     f = FileManager(config)
-    res = create_upstream(f)
-    for k, v in res.items():
+    streams = create_streams(f)
+    for k, v in streams.items():
         print(k, v)
+
