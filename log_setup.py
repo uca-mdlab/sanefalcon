c7ec1e79b2d41c0182d992c8c104f1f723d36766
marcomilanesio <milanesio.marco@gmail.com>
2020-02-05 15:19:12 +0100

    rotating file handler

--- log_setup.py
+++ log_setup.py
@@ -1,13 +1,13 @@
 import logging
-# from logging.handlers import RotatingFileHandler
+from logging.handlers import RotatingFileHandler
 
 formatter = logging.Formatter('%(asctime)s - %(name)-21s - %(levelname)-8s: %(message)s')
 
 
 def setup_logger(name, log_file, level=logging.DEBUG):
 
-    #handler = RotatingFileHandler(log_file, maxBytes=1e7, backupCount=10)
-    handler = logging.FileHandler(log_file)
+    handler = RotatingFileHandler(log_file, maxBytes=1e7, backupCount=10)
+    # handler = logging.FileHandler(log_file)
 
     handler.setFormatter(formatter)
 
