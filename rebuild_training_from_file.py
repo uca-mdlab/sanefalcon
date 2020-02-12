ea80e76ee4bf2e2d158a1e644d92901d54ec8f16
marcomilanesio <milanesio.marco@gmail.com>
2020-02-12 17:00:11 +0100

    rebuilding run from file

---
+++ rebuild_training_from_file.py
@@ -0,0 +1,24 @@
+import os
+import sys
+from collections import defaultdict
+
+data_dir = '/home/mdlab/storage/data_nice/dedup'
+trainingdir = '/home/mdlab/storage/sanefalcon/training'
+batches = defaultdict(list)
+with open(sys.argv[1], 'r') as infile:
+    lines = [x.strip() for x in infile.readlines()]
+
+for line in lines:
+    name, f = line.split('/')
+    batches[name].append(os.path.join(data_dir, f))
+
+for k, v in batches.items():
+    current_dir = os.path.join(trainingdir, k)
+    try:
+        os.makedirs(current_dir)
+    except FileExistsError:
+        pass
+
+    for fname in v:
+        os.symlink(fname, os.path.join(current_dir, os.path.basename(fname)))
+        os.symlink(fname + '.bai', os.path.join(current_dir, os.path.basename(fname) + '.bai'))
