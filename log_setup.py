a1232aa1c8f54e3ca266c5c382338e8e92ceaa44
marcomilanesio <milanesio.marco@gmail.com>
2020-02-25 14:13:08 +0100

    add streamhandler to sys.stdout
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- log_setup.py
+++ log_setup.py
@@ -1,13 +1,15 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import sys
 
 formatter = logging.Formatter('%(asctime)s - %(name)-21s - %(levelname)-8s: %(message)s')
 
 
 def setup_logger(name, log_file, level=logging.DEBUG):
-
-    # handler = RotatingFileHandler(log_file, maxBytes=1e7, backupCount=20)
-    handler = logging.FileHandler(log_file)
+    if log_file:
+        handler = logging.FileHandler(log_file)
+    else:
+        handler = logging.StreamHandler(sys.stdout)
 
     handler.setFormatter(formatter)
 
