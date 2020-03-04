70e88803972d9bb6646865b55593941c7c1372c7
marcomilanesio <milanesio.marco@gmail.com>
2020-03-04 16:39:22 +0100

    towards pysam integration (and samtools removing)
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- manager/rsp.py
+++ manager/rsp.py
@@ -4,6 +4,9 @@ import subprocess
 import concurrent.futures
 
 from log_setup import setup_logger
+import pysam
+import pickle
+from retro import in_mem_retro
 
 logger = setup_logger(__name__, 'logs/manager.log')
 
@@ -12,14 +15,42 @@ class RspBuilder:
     def __init__(self, config):
         self.samtools = config['default']['samtools']
 
+    def prepare_fwd_new(self, bamfile, outfile, chrom):
+        reads = []
+        infile = pysam.AlignmentFile(bamfile, 'rb')
+        for read in infile.fetch(f'chr{chrom}'):
+            if not read.is_reverse and read.mapping_quality >= 1:
+                start = read.reference_start + 1  # pysam is 0-indexed. samtools 1-indexed
+                length = read.reference_length
+                reads.append((start, length))
+        return reads
+
     def prepare_fwd(self, bamfile, outfile, chrom):
-        one = [self.samtools, "view", bamfile, "chr{}".format(chrom), "-F", "20", "-q", "1"]
-        two = [sys.executable, "retro.py", "-fwd"]
-        p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
-        with open(outfile, 'w') as out_:
-            p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=out_)
-            output = p2.communicate()[0]
-        return outfile
+        reads = self.prepare_fwd_new(bamfile, outfile, chrom)
+        in_mem_retro(reads, outfile, flag=True)
+
+    def prepare_rev_new(self, bamfile, outfile, chrom):
+        reads = []
+        infile = pysam.AlignmentFile(bamfile, 'rb')
+        for read in infile.fetch(f'chr{chrom}'):
+            if read.is_reverse and read.mapping_quality >= 1:
+                start = read.reference_start
+                length = read.reference_length + 1
+                reads.append((start, length))
+        return reads
+
+    def prepare_rev_tmp(self, bamfile, outfile, chrom):
+        reads = self.prepare_rev_new(bamfile, outfile, chrom)
+        in_mem_retro(reads, outfile, False)
+
+    # def prepare_fwd(self, bamfile, outfile, chrom):
+    #     one = [self.samtools, "view", bamfile, "chr{}".format(chrom), "-F", "20", "-q", "1"]
+    #     two = [sys.executable, "retro.py", "-fwd"]
+    #     p1 = subprocess.Popen(one, stdout=subprocess.PIPE)
+    #     with open(outfile, 'w') as out_:
+    #         p2 = subprocess.Popen(two, stdin=p1.stdout, stdout=out_)
+    #         output = p2.communicate()[0]
+    #     return outfile
 
     def prepare_rev(self, bamfile, revoutfile, chrom):
         one = [self.samtools, "view", bamfile, "chr{}".format(chrom), "-f", "16", "-F", "4", "-q", "1"]
@@ -76,3 +107,14 @@ class RspBuilder:
             return [os.path.join(rspfolder, fname) for fname in os.listdir(rspfolder)]
         else:
             return outfiles
+
+
+if __name__ == '__main__':
+    config = {'default': {'samtools': '/user/mmilanes/home/sw/samtools-1.9/samtools'}}
+    infile = '/data/tempff/bam/p1_065_dedup.bam'
+    chrom = 1
+    rspbuilder = RspBuilder(config)
+    #rspbuilder.prepare_fwd_tmp(infile, '/data/tempff/1.tmp.fwd', chrom)
+    rspbuilder.prepare_rev_tmp(infile, '/data/tempff/1.tmp.rev', chrom)
+    #fwdfile = rspbuilder.prepare_fwd(infile, '/data/tempff/1.fwd', chrom)
+    #revfile = rspbuilder.prepare_rev(infile, '/data/tempff/1.rev', chrom)
