dd8e3132c877d5ce0e33fbe6eb3db7883adb99ec
marcomilanesio <milanesio.marco@gmail.com>
2020-03-04 16:40:02 +0100

    in_mem_retro: avoid subprocessing
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- retro.py
+++ retro.py
@@ -8,6 +8,40 @@ startColumn = 3
 sequenceColumn = 9
 
 
+def in_mem_retro(arr, outfile, flag=True):
+    out = open(outfile, 'a')
+
+    def flush(read_buffer):
+        stairSize = len(read_buffer)
+        if stairSize <= threshold or threshold < 0:
+            for line in read_buffer:
+                if flag:  # .fwd files: only start position
+                    out.write(f'{line[0]}\n')
+                else:  # .rev files: start + length of sequence
+                    out.write(f'{line[1]}\n')
+
+    read_buffer = []
+    prev_start = 0
+
+    for cur_start, cur_length in arr:
+        if flag:
+            length = cur_length - 1
+        else:
+            length = cur_length
+        rev_l = cur_start + length  # (start + len sequence) for .rev files
+        if minShift < cur_start - prev_start:
+            flush(read_buffer)
+            read_buffer = []
+        if len(read_buffer) == 0 or cur_start != read_buffer[-1][0]:
+            # Normal ndups will be appended here
+            read_buffer.append((cur_start, rev_l))
+
+        prev_start = cur_start
+
+    flush(read_buffer)
+    out.close()
+
+
 def retro(flag=True):
     """
     flag = True -> fwd files
