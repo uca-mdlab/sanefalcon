6d035f44615fd96803d3a4b55f236f6615855526
marcomilanesio <milanesio.marco@gmail.com>
2020-03-10 15:13:05 +0100

    improved bash syntax
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- redoworkingdir.sh
+++ redoworkingdir.sh
@@ -5,23 +5,26 @@ SOURCE=$1
 
 echo "Rebuilding training tree from ${SOURCE}"
 
-for d in $SOURCE/training/*/ ; do
-    name=`basename $d`
-    mkdir $WORKDIR/$name
-    pushd $WORKDIR/$name
+for d in "$SOURCE"/training/*/ ; do
+    name=$(basename "$d")
+    mkdir $WORKDIR/"$name"
+    pushd $WORKDIR/"$name" || { echo "Failure"; exit 1; }
     if [ "$name" == "profiles" ]; then
-        for f in `ls $SOURCE/training/$name/p*`; do
-            ln $f
+        for f in "$SOURCE"/training/"$name"/p*; do
+            [[ -e "$f" ]] || break
+            ln "$f" .
         done
-        for f in `ls $SOURCE/training/$name/s*`; do
-            ln $f
+        for f in "$SOURCE"/training/"$name"/s*; do
+            [[ -e "$f" ]] || break
+            ln "$f" .
         done
     else
-        for f in `ls $SOURCE/training/$name/*.*`; do
-            ln $f
+        for f in "$SOURCE"/training/"$name"/*.*; do
+            [[ -e "$f" ]] || break
+            ln "$f" .
         done
     fi
-    popd
+    popd || { echo "Failure"; exit 1; }
 
 done
 echo "Done. Training tree in ${SOURCE} restored in ${WORKDIR}"
