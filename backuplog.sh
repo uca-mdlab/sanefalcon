050cd3f343d695f0130de517814eaad44b53a132
marcomilanesio <milanesio.marco@gmail.com>
2020-03-10 15:04:36 +0100

    syntax
    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- backuplog.sh
+++ backuplog.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-NOW=`date +'%d-%m-%Y_%Hh%M'`
+NOW=$(date +'%d-%m-%Y_%Hh%M')
 BKPDIR="backup${NOW}"
 
 mkdir -p $BKPDIR
 echo "Packing and deleting from app folder"
-tar czvf ${BKPDIR}/images.tar.gz testingNucleosome.csv* train_curvefit.png trainingNucleosome.csv*
-tar czvf ${BKPDIR}/logs.tar.gz logs/*
-grep 'ff(' logs/sanefalcon.log > ${BKPDIR}/results.ff
+tar czvf "${BKPDIR}"/images.tar.gz testingNucleosome.csv* train_curvefit.png trainingNucleosome.csv*
+tar czvf "${BKPDIR}"/logs.tar.gz logs/*
+grep 'ff(' logs/sanefalcon.log > "${BKPDIR}"/results.ff
 
 
 rm testingNucleosome.csv*
