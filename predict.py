499bee1d33141981c0d3e01f0f60017addf285e6
mdlab <david.pratella@univ-cotedazur.fr>
2020-07-15 12:37:28 +0000

    removed ordereddict trick: no gain

--- predict.py
+++ predict.py
@@ -9,43 +9,40 @@
 
 
 import sys
-from collections import OrderedDict
 
 reference_file_name = sys.argv[1]
 profile_file_name = sys.argv[2]
 
 nuclData=dict()
 with open(reference_file_name, 'r') as referenceFile:
-	correlations = [float(x) for x in referenceFile.readline().split(" ")]
-	scalars = [float(x) for x in referenceFile.readline().split(" ")]
+        correlations = [float(x) for x in referenceFile.readline().split(" ")]
+        scalars = [float(x) for x in referenceFile.readline().split(" ")]
 #print len(correlations),scalars
-	
+
 with open(profile_file_name, 'r') as profileFile:
-	# In these readlines -1 to drop the 'end of line' comma from a previous awk
-	my_lines = profileFile.readlines()
+        # In these readlines -1 to drop the 'end of line' comma from a previous awk
+        my_lines = profileFile.readlines()
 profileFile.close()
 profile = [float(x) for x in my_lines[0].split(",") if x != '\n']
 profile.reverse()
 
 profile.extend([float(x) for x in my_lines[1].split(",") if x != '\n'])
+#print('--->', len(correlations), len(profile))
+if len(profile) == len(correlations) + 1:
+    profile = profile[:-1]
 
-no_dup_profile = list(OrderedDict.fromkeys(profile))
-try:
-	assert len(correlations) == len(no_dup_profile)
-except AssertionError:
-	print('Data not aligned on ', reference_file_name)
-	exit(1)
 
-totalReads = sum(no_dup_profile)
-normProfile = [x/totalReads for x in no_dup_profile]
-	
+#print(len(profile), len(correlations), len(no_dup_profile))
+totalReads = sum(profile)
+normProfile = [x/totalReads for x in profile]
+
 summed = 0
 for i, val in enumerate(normProfile):
-	summed += val*correlations[i]
+        summed += val*correlations[i]
 fetalFraction = scalars[0]*summed+scalars[1]
 
 if len(correlations) == len(normProfile):
-	print("Fetal Fraction:\t", fetalFraction)
+        print("Fetal Fraction:\t", fetalFraction)
 else:
-	print("ERROR: correlation and sample profiles are not aligned")
+        print("ERROR: correlation and sample profiles are not aligned")
 print("Nucleosome Profile:", "\t".join([str(x) for x in normProfile]))
